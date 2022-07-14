e90c035d6013234273310a2cca1d8c01ce61008a
Ryan Neill <ryan.neill@tpicap.com>
2022-07-14 18:48:30 +0100

    Fully implemented methods in UnReaLService

--- UnReaL/Repository/UnReaLService.cs
+++ UnReaL/Repository/UnReaLService.cs
@@ -1,27 +1,37 @@
-﻿using UnReaL.Models;
+﻿using UnReaL.Database;
+using UnReaL.Models;
 
 namespace UnReaL.Repository
 {
     public class UnReaLService : IUnReaLService
     {
-        public ShortURL GetById(int id)
+        private readonly UnReaLContext _context;
+        private readonly IBijectionService _bijectionService;
+
+        public UnReaLService(UnReaLContext context, IBijectionService bijectionService)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _bijectionService = bijectionService;
         }
 
-        public ShortURL GetByPath(string path)
-        {
-            throw new NotImplementedException();
-        }
+        public ShortURL GetById(int id) => _context.ShortURLs.Find(id);
 
-        public ShortURL GetByUrl(string url)
-        {
-            throw new NotImplementedException();
-        }
+        public ShortURL GetByPath(string path) => _context.ShortURLs.Find(_bijectionService.Decode(path));
+            
+
+        public ShortURL GetByUrl(string url) 
+            => _context.ShortURLs.Where(shortUrl => shortUrl.Url == url).FirstOrDefault();
 
         public int Save(ShortURL shortUrl)
         {
-            throw new NotImplementedException();
+            // Check for duplicates
+            var existing = _context.ShortURLs.Where(url => url.Url == shortUrl.Url).FirstOrDefault();
+            if (existing != null) { return existing.Id; }
+
+            _context.ShortURLs.Add(shortUrl);
+            _context.SaveChanges();
+
+            return shortUrl.Id;
         }
     }
 }
