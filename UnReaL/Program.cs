0287169092dcb722cf1814be99ffcc6106b499e9
Ryan Neill <ryan.neill@tpicap.com>
2022-07-15 18:00:08 +0100

    Changed authentication from windows to sql server and configured roles/permissions

--- UnReaL/Program.cs
+++ UnReaL/Program.cs
@@ -4,12 +4,12 @@ using UnReaL.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 var root = Directory.GetCurrentDirectory();
+var connStr = $"Server=(localdb)\\mssqllocaldb;Database={root}\\UnReaL.mdf;User Id=admin;password=aWCgNyuqYx6mhoudcw@VmmsFT7_&#$!~<Ufbxtovfsc3m;Trusted_Connection = True;";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddMvc();
-builder.Services.AddDbContext<UnReaLContext>(options 
-    => options.UseSqlServer($"Server = (localdb)\\mssqllocaldb; Database = {root}\\UnReaL.mdf; Trusted_Connection = True;"));
+builder.Services.AddDbContext<UnReaLContext>(options => options.UseSqlServer(connStr));
 builder.Services.AddScoped<IUnReaLService, UnReaLService>();
 builder.Services.AddScoped<IBijectionService, BijectionService>();
 
